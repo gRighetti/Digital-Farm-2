9128e79ef2c08d08e48b3e1ce1d1e9b9a6f2097f
jonamenabar <jonamenabar@hotmail.com>
2015-10-13 16:05:38 -0300

    Se agrego servo
    se agrego serio y se cambio la variable que muestra en lcd

--- dth11_primera_prueba/dth11_primera_prueba.ino
+++ dth11_primera_prueba/dth11_primera_prueba.ino
@@ -1,22 +1,36 @@
 
 #include "DHT.h" //cargamos la librería DHT
 #include <LiquidCrystal.h>
+#include <Servo.h> 
 
+//PANTALLA LCD
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+//SERVO
+Servo myservo;  // create servo object to control a servo 
+                // twelve servo objects can be created on most boards
+ int pos = 0;    // variable to store the servo position 
+
+//SENSOR DE TEMPERATURA
 #define DHTPIN 13 //Seleccionamos el pin en el que se //conectará el sensor
 #define DHTTYPE DHT11 //Se selecciona el DHT11 (hay //otros DHT)
 DHT dht(DHTPIN, DHTTYPE); //Se inicia una variable que será usada por Arduino para comunicarse con el sensor
+
 void setup() {
-Serial.begin(9600); //Se inicia la comunicación serial 
-dht.begin(); //Se inicia el sensor
 
+   //CONFIGURACION SENSOR HUMEDAD 
+   Serial.begin(9600); //Se inicia la comunicación serial 
+   dht.begin(); //Se inicia el sensor
 
-// set up the LCD's number of columns and rows:
+  //CONFIGURACION LCD
+  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("Digital farm");
+
+  //CONFIGURACION SERVO
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 void loop() {
 float h = dht.readHumidity(); //Se lee la humedad
@@ -43,7 +57,7 @@ lcd.setCursor(0, 0);
 lcd.print("Temperatura: ");
 
 lcd.setCursor(0,1);
-lcd.print(h);
+lcd.print(t);
 //Serial.println(t);
 delay(2000); //Se espera 2 segundos para seguir leyendo //datos
 lcd.clear();
@@ -55,6 +69,27 @@ lcd.print(humedad2);
 delay(2000);
 lcd.clear();
 
+
+/*
+//SERVO - HAY QUE ELEGIR UNA DE LAS DOS, DEPENDE COMO DISPONGAMOS EL SERVO.
+while(condicion){
+  delay(1000);
+  
+for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);// waits 15ms for the servo to reach the position 
+  }
+               } 
+  
+while(condicion2){  
+  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+                 }
+                 */
 
 }
 
